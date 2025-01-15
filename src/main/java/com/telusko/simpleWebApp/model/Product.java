278e5ad2b507acc5bbcfa9e41bf43f33868640b8
Pratishtha <pratishtha.kumari-st@adityabirla.com>
2025-01-15 11:11:45 +0530

    setup db using @Bean

--- src/main/java/com/telusko/simpleWebApp/model/Product.java
+++ src/main/java/com/telusko/simpleWebApp/model/Product.java
@@ -2,6 +2,7 @@ package com.telusko.simpleWebApp.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @NoArgsConstructor
+@Table
 public class Product {
 
     @Id
